798c6b4660e9447b9b50582fc3f5bb1959aa6be9
Kavindyasenarathna <dkavindya17@gmail.com>
2024-06-10 08:40:55 +0530

    Add files via upload

---
+++ ControlStructure2.java
@@ -0,0 +1,36 @@
+public class ControlStructure2 {
+
+    public static void main (String []aegs){
+
+    //summation of all the numbers from 1 to 10//
+
+    //using for loop//
+
+    System.out.println("");
+
+    int sum=0;
+
+    for(int i=1; i<=10; i++){
+        sum+=i;
+    }
+    System.out.println("the sum of numbers from 1 to 10="+sum);
+
+    System.out.println("");
+
+    //using while loop//
+
+    int sum=0;
+    int x=1;
+
+    while(x<=10){
+        sum+=x;
+        x++;
+    }
+    System.out.println("the sum of numbers from 1 to 10="+sum);
+
+
+
+
+
+} 
+}
