201f377cf757bb1b14063adf9a156e2206a9712b
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-01 02:36:34 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+public class Welcome {
+    public static void main (String[]args){
+        System.out.println("Welcome to \'UWU\'My University is;\n");
+        System.out.println("located in \t:\'Badulla\".\n");
+        System.out.println("started in \t:2008\b5.\n");
+        System.out.println("has about \\ 3000\\students.\n");
+    }
+    
+}
