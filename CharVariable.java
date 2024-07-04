a93917e17c630e35e229dc58475402f015c9f495
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-04 22:29:02 +0530

    Add files via upload

---
+++ CharVariable.java
@@ -0,0 +1,10 @@
+public class CharVariable {
+
+    public static void main (String[]args){
+
+        char ch1 = 'a';
+        char ch2 = 'b';
+        System.out.println("\'Character1\'is:"+ ch1);
+        System.out.println("\'Character2\'is:"+ ch2);  
+}
+}
