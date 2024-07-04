29b08d99d30376ca9cdee50432e04a40cf991392
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-04 22:28:39 +0530

    Add files via upload

---
+++ FloatVariable.java
@@ -0,0 +1,11 @@
+public class FloatVariable {
+
+    public static void main(String[]args){
+
+       float x = 3.24f;
+       float y = 1.28f;
+       System.out.println("the sum of the x and y is:"+ (x+y));
+
+    }
+    
+}
