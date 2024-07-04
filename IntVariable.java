1191270baeb9013a07eb974d341922a9e69d4c19
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-04 22:28:17 +0530

    Add files via upload

---
+++ IntVariable.java
@@ -0,0 +1,11 @@
+public class IntVariable {
+
+    public static void main(String[]args){
+
+        int a = 10;
+        System.out.println("the value of the variable \'a\' is:"+ a);
+      
+     
+    }
+    
+}
