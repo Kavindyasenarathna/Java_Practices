5ce2248085e8b9f2ec1dab4d5b9bf6e7ad54d6c5
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-06 08:51:39 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,13 @@
+
+
+public class Constants {
+
+    public static void main (String[]args){
+
+        final float PI = 3.14f;
+        float radius = 5.68f;
+        System.out.println("the area is:"+(PI*radius*radius));
+        
+    }
+    
+}
