815b4de7a145e6753e951957b5524f24bf9e38ad
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-06 08:51:13 +0530

    Add files via upload

---
+++ DataTypeConversion2.java
@@ -0,0 +1,12 @@
+public class DataTypeConversion2 {
+
+    public static void main(String[]args){
+
+        //type casting//
+
+        float total = 126.82f;
+        int sum = (int)total;
+        System.out.println("the sum is:"+ sum);
+    }
+    
+}
