0ea7aff7d02ecece95d06cd2da6a2846d1902af3
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-06 08:50:46 +0530

    Add files via upload

---
+++ DataTypeConversion1.java
@@ -0,0 +1,15 @@
+public class DataTypeConversion1 {
+
+    public static void main(String[]args){
+
+        //promotion//
+
+        float total = 126.82f;
+        int count = 8 ;         
+        float result = total / count;
+        System.out.println("the result is:"+ result);
+
+     }
+
+    
+}
