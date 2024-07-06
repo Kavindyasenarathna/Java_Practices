f5a11625133b9c63f26ed55bb33231ba5d9b511d
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-06 08:50:18 +0530

    Add files via upload

---
+++ DataTypeConversion.java
@@ -0,0 +1,16 @@
+public class DataTypeConversion {
+
+    public static void main(String[]args){
+
+        //assignment//
+
+        int x = 10;
+        float f;
+        f = x ;
+        System.out.println("the value of\'f\'is:"+ f);
+
+            
+        }
+    }
+    
+    
