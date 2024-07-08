1aee1a707f442a4c9a422e288a191ee473211af9
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-08 09:42:30 +0530

    Add files via upload

---
+++ ConditionalOperator.java
@@ -0,0 +1,14 @@
+public class ConditionalOperator {
+
+    public static void main (String[]args){
+
+        int num1 =3;
+        int num2 =4;
+        int largest;
+
+        largest = (num1>num2)? num1:num2;
+        System.out.println("the largest number is:"+ largest);
+        
+    }
+    
+}
