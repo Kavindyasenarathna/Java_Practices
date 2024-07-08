4bf988998156fb7caa81bb742e526192e9e4fe57
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-08 09:40:26 +0530

    Add files via upload

---
+++ ArithmaticOperators.java
@@ -0,0 +1,16 @@
+public class ArithmaticOperators {
+
+    public static void main (String[]args){
+
+        int a = 5;
+        int b = 3;
+
+        System.out.println("The addition is:"+ (a+b));
+        System.out.println("The subtraction is:"+ (a-b));
+        System.out.println("The multiplicatin is:"+ (a*b));
+        System.out.println("The division is:"+ (a/b));
+        System.out.println("The remainder is:"+ (a%b));
+    
+    }
+    
+}
