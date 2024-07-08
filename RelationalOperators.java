3bc396b1dcb847aabe87ed79445e93fbbbe9ad9f
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-08 09:40:57 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,16 @@
+public class RelationalOperators {
+
+    public static void main (String[]args){
+
+        int a = 4;
+        int b = 3;
+
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+    }
+    
+}
