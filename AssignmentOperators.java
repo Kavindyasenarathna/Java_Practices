b5774777daede84bfe4945df867052deaa77b3a0
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-08 09:41:48 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,19 @@
+public class AssignmentOperators {
+
+    public static void main (String[]args){
+
+        int a = 2;
+
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(a+=1);
+        System.out.println(a-=1);
+        System.out.println(a*=4);
+        System.out.println(a/=6);
+        System.out.println(-a);
+
+    }
+    
+}
