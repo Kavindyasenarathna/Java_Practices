c241ba74397d2221591924bba646ffc03fde6200
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-12 01:02:02 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,12 @@
+public class Dog {
+
+    int age = 4;
+    void run(){
+
+    }
+    public static void main(String[]args){
+
+        Dog scooby = new Dog();
+        System.out.println("The age of scooby is:"+scooby.age);
+    }
+}
