af4e353e158c57fa202ef966280d76a8d0cbf651
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-12 01:03:41 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,13 @@
+public class StringLength {
+
+    public static void main(String[]args){
+
+        String uwu = "Uva Wellassa University";
+        int len;
+        len  = uwu.length();
+
+        System.out.println("the length of the String is:"+ len);
+        
+    }
+    
+}
