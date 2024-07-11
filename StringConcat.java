2b67d71e76b616e43cd3ef252893c55537968bbf
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-12 01:04:16 +0530

    Add files via upload

---
+++ StringConcat.java
@@ -0,0 +1,11 @@
+public class StringConcat {
+
+    public static void main(String[]args){
+
+     System.out.println("Uva Wellassa "+ "University");
+
+     System.out.println("Uva Wellassa ".concat("University") );
+     
+    }
+    
+}
