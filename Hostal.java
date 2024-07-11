e51deaa636fff14e3f44f791f56bce8bf6462ed3
Kavindyasenarathna <dkavindya17@gmail.com>
2024-07-12 01:02:29 +0530

    Add files via upload

---
+++ Hostal.java
@@ -0,0 +1,17 @@
+public class Hostal {
+
+    float length = 12.56f;
+    float width = 3.12f;
+
+    public static void main(String[]args){
+           Hostal corel_beauty = new Hostal();
+           Hostal silver_tips = new Hostal();
+           Hostal blue_sapphire = new Hostal();
+           Hostal cattleya = new Hostal();
+
+    System.out.println("the height of cattleya is:"+ cattleya.length);
+    System.out.println("the width of the cattleya is:"+ cattleya.width);      
+
+    }         
+    
+}
